d2c49fb3c5a50b845315ccfee5cea5b95323937f
Me1onMan <30theflame05@gmail.com>
2026-02-18 10:48:17 +0300

    feature: make simple footer

---
+++ src/constants/navLinksFooter.ts
@@ -0,0 +1,40 @@
+import type { INavLink } from "./navLinks";
+
+export const navLinksFooter: Array<INavLink> = [
+  {
+    title: "Главная",
+    link: "https://zeleniybor.by",
+  },
+  {
+    title: "Номера и цены",
+    link: "https://zeleniybor.by/rooms",
+  },
+  {
+    title: "Бронирование",
+    link: "https://zeleniybor.by/online-booking",
+  },
+  {
+    title: "Лечение",
+    link: "https://zeleniybor.by/treatment",
+  },
+  {
+    title: "Путевки",
+    link: "https://zeleniybor.by",
+  },
+  {
+    title: "Спорт и доп. услуги",
+    link: "https://zeleniybor.by/sport-and-additional-services",
+  },
+  {
+    title: "Лагерь",
+    link: "https://zeleniybor.by/camp",
+  },
+  {
+    title: "Контакты",
+    link: "https://zeleniybor.by/contacts",
+  },
+  {
+    title: "О санатории",
+    link: "https://zeleniybor.by/about",
+  },
+];
