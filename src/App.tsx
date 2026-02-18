add7e61c44d3736d6bae69a68f60074c71d91097
Me1onMan <30theflame05@gmail.com>
2026-02-18 10:56:04 +0300

    feature: add components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Header from "@components/Header/Header";
 import ErrorBoundary from "@utils/ErrorBoundary";
 import WelcomeSlider from "@components/WelcomeSlider/WelcomeSlider";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <ErrorBoundary fallback={<div>Header Error Boundary</div>}>
         <Header />
+        <WelcomeSlider />
+        <Footer />
       </ErrorBoundary>
-      <WelcomeSlider />
     </>
   );
 }
